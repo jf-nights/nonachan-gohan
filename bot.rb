e0689deded05a9ed3e5efdd0c1be14497b9cbbff
jf712@ring <jf712@kmc.gr.jp>
2015-06-25 08:11:56 +0900

    a~a

--- bot.rb
+++ bot.rb
@@ -9,6 +9,15 @@ ako = Cinch::Bot.new do
   end
 
   on :message do |m|
+    if m.channel.name == '#nona-food-memo' && m.user.nick == 'nona7'
+      date = Time.now.strftime("%Y-%m-%d")
+      time = m.time.strftime("%H:%M")
+      input = m.message
+
+      open("/home/jf712/local/log/nonachan-gohan/#{date}.log", "a") do |file|
+        file.puts("#{time} #{input}")
+      end
+    end
   end
 
 end
